262624a01d1f917cf759fb189dcac992422c8396
Christopher Dorst <chris@dorst.email>
2017-12-28 00:30:32 -0600

    Add AccountRepository

---
+++ DevOps.VersionControl.Structures.GitHub/AccountRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace DevOps.VersionControl.Structures.GitHub
+{
+    public struct AccountRepository
+    {
+        private const string GitHub = "https://github.com";
+
+        public AccountRepository(string account, string repository)
+        {
+            Account = account;
+            Repository = repository;
+        }
+
+        public string Account { get; set; }
+        public string Repository { get; set; }
+
+        public string Uri => $"{GitHub}/{Account}/{Repository}";
+    }
+}
